5bb99ef58f0a5e0e25f70b42d7f4700879fd1a47
VitaValeeva <vdemahina@yandex.ru>
2024-03-31 20:36:15 +0300

    week-2 homework

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,20 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el)); // выведет каждый элемент массива
  */
-function forEach() {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
+
+// 2 вариант решения
+
+// function forEach(array, fn) {
+//   for (const [ix, el] of array.entries()) {
+//     fn(el, ix, array);
+//   }
+// }
+
 
 /*
  Задание 2:
@@ -21,7 +34,28 @@ function forEach() {}
    const newArray = map([1, 2, 3], (el) => el ** 2);
    console.log(newArray); // выведет [1, 4, 9]
  */
-function map() {}
+function map(array, fn) {
+  const newArray = [];
+
+  for (const [ix, el] of array.entries()) {
+    newArray.push(fn(el, ix, array));
+  }
+
+  return newArray;
+}
+
+// 2 вариант решения
+
+// function map(array, fn) {
+//   let newArray = [];
+
+//     for (let i = 0; i < array.length; i++) {
+//         newArray[i] = fn(array[i], i, array);
+//     }
+//     return newArray;
+// }
+
+
 
 /*
  Задание 3:
@@ -33,7 +67,34 @@ function map() {}
    const sum = reduce([1, 2, 3], (all, current) => all + current);
    console.log(sum); // выведет 6
  */
-function reduce() {}
+  function reduce(array, fn, initial) {
+    let startIndex = 0;
+    let all = initial;
+
+    if (initial === undefined) {
+      startIndex = 1;
+      all = array[0];
+    }
+
+    for (let i = startIndex; i < array.length; i++) {
+      all = fn(all, array[i], i, array);
+    }
+
+    return all;
+  }   
+  
+//   2 вариант решения
+
+//   function reduce(array, fn, initial) {
+//     let all = initial || array[0],
+//     i = initial ? 0 : 1;
+
+//     for (; i < array.length; i++) {
+//         all = fn(all, array[i], i, array);
+//     }
+
+//     return all;
+// }
 
 /*
  Задание 4:
@@ -44,6 +105,19 @@ function reduce() {}
    const keys = upperProps({ name: 'Сергей', lastName: 'Петров' });
    console.log(keys) // выведет ['NAME', 'LASTNAME']
  */
-function upperProps() {}
+  function upperProps(obj) {
+    return Object.keys(obj).map(k => k.toUpperCase());
+  }
+
+// 2 вариант решения
+
+// function upperProps(obj) {
+//   let arr = [];
+//   for (let key in obj) {
+//       key = key.toUpperCase();
+//       arr.push(key);
+//   }
+//   return arr;
+// }
 
 export { forEach, map, reduce, upperProps };
