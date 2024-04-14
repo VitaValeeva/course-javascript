13f6c8b210f273406993ee524c234b1afa7b1c39
VitaValeeva <vdemahina@yandex.ru>
2024-04-15 02:10:00 +0300

    week-4 homework

--- projects/loft-photo/index.js
+++ projects/loft-photo/index.js
@@ -1,9 +1,8 @@
 import pages from './pages';
+import mainPage from './mainPage';
+import loginPage from './loginPage';
 import('./styles.css');
 
-const pageNames = ['login', 'main', 'profile'];
-
-document.addEventListener('click', () => {
-  const pageName = model.getRandomElement(pageNames);
-  pages.openPage(pageName);
-});
+pages.openPage('login');
+loginPage.handleEvents();
+mainPage.handleEvents();
